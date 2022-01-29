7e323770898fb26be8b0fd8103ad9dcab5497f22
Güven Karanfil <guvenkaranfil235@gmail.com>
2022-01-30 00:44:09 +0300

    refactor: run prettier

--- top100/medium/index.js
+++ top100/medium/index.js
@@ -1,44 +1,48 @@
 var convert = function (s, numRows) {
   if (numRows === 1) return s;
 
-  let res = '';
+  let res = "";
   for (let r = 0; r < numRows; r++) {
     let increment = 2 * (numRows - 1);
     for (let i = r; i < s.length; i += increment) {
       res += s[i];
 
       if (r > 0 && r < numRows - 1 && i + increment - 2 * r < s.length) {
-        res += s[i + increment - 2 * r]
+        res += s[i + increment - 2 * r];
       }
     }
   }
 
-  return res
+  return res;
 };
 
 var reverse = function (x) {
-  let MIN = -2147483648 // -2^31
-  let MAX = 2147483647  // 2^31 - 1
+  let MIN = -2147483648; // -2^31
+  let MAX = 2147483647; // 2^31 - 1
 
-  let res = 0
+  let res = 0;
   while (x) {
     let digit = x % 10;
-    x = parseInt(x / 10)
+    x = parseInt(x / 10);
 
-    if (res > Math.floor(MAX / 10) ||
-      (res === Math.floor(MAX / 10) && digit >= MAX % 10)) {
-      return 0
+    if (
+      res > Math.floor(MAX / 10) ||
+      (res === Math.floor(MAX / 10) && digit >= MAX % 10)
+    ) {
+      return 0;
     }
 
-    if (res < Math.floor(MIN / 10) ||
-      (res == Math.floor(MIN / 10) && digit <= MIN % 10)) {
-      return 0
+    if (
+      res < Math.floor(MIN / 10) ||
+      (res == Math.floor(MIN / 10) && digit <= MIN % 10)
+    ) {
+      return 0;
     }
 
-    res = (res * 10) + digit
+    res = res * 10 + digit;
   }
 
-  return res
+  return res;
 };
 
 var myAtoi = function (s) {
@@ -49,48 +53,48 @@ var myAtoi = function (s) {
     4- Check the MAX / MIN
   */
 
-  let MIN_INT = -(2 ** 31)
-  let MAX_INT = 2 ** 31 - 1
+  let MIN_INT = -(2 ** 31);
+  let MAX_INT = 2 ** 31 - 1;
 
-  let i = 0
-  let res = 0
-  let negative = 1
+  let i = 0;
+  let res = 0;
+  let negative = 1;
 
   // 1. Whitespace
-  while (i < s.length && s[i] == ' ') {
-    i += 1
+  while (i < s.length && s[i] == " ") {
+    i += 1;
   }
 
   // 2. + / - symbol
-  if (i < s.length && s[i] == '-') {
-    i += 1
-    negative = -1
-  } else if (i < s.length && s[i] == '+') {
-    i += 1
+  if (i < s.length && s[i] == "-") {
+    i += 1;
+    negative = -1;
+  } else if (i < s.length && s[i] == "+") {
+    i += 1;
   }
 
   // 3. Check number digits
-  let digits = new Set('0123456789')
+  let digits = new Set("0123456789");
   while (i < s.length && digits.has(s[i])) {
-    res = res * 10 + parseInt(s[i])
-    i += 1
+    res = res * 10 + parseInt(s[i]);
+    i += 1;
   }
 
   // 4. Check the MAX / MIN int
-  res = res * negative
+  res = res * negative;
   if (res < 0) {
-    return Math.max(res, MIN_INT)
+    return Math.max(res, MIN_INT);
   }
 
-  return Math.min(res, MAX_INT)
+  return Math.min(res, MAX_INT);
 };
 
 var isPalindrome = function (x) {
-  if (x < 0) return false
+  if (x < 0) return false;
 
-  let divider = 1
+  let divider = 1;
   while (x >= 10 * divider) {
-    divider = divider * 10
+    divider = divider * 10;
   }
 
   while (x > 0) {
@@ -99,9 +103,9 @@ var isPalindrome = function (x) {
 
     if (x % 10 != Math.floor(x / divider)) return false;
 
-    x = x % divider
-    x = Math.floor(x / 10)
-    divider = divider / 100
+    x = x % divider;
+    x = Math.floor(x / 10);
+    divider = divider / 100;
   }
 
   return true;
@@ -111,28 +115,27 @@ var isMatch = function (s, p) {
   let cache = new Map();
 
   function dfs(i, j) {
-    if (cache.has(`${i}:${j}`)) return cache.get(`${i}:${j}`)
+    if (cache.has(`${i}:${j}`)) return cache.get(`${i}:${j}`);
 
-    if (i >= s.length && j >= p.length) return true
+    if (i >= s.length && j >= p.length) return true;
 
-    if (j >= p.length) return false
+    if (j >= p.length) return false;
 
-    let match = i < s.length && (s[i] === p[j] || p[j] === '.')
+    let match = i < s.length && (s[i] === p[j] || p[j] === ".");
 
-    if (j + 1 < p.length && p[j + 1] === '*') {
-      cache.set(`${i}:${j}`, (dfs(i, j + 2) || match && dfs(i + 1, j)))
-      return cache.get(`${i}:${j}`)
+    if (j + 1 < p.length && p[j + 1] === "*") {
+      cache.set(`${i}:${j}`, dfs(i, j + 2) || (match && dfs(i + 1, j)));
+      return cache.get(`${i}:${j}`);
     }
 
     if (match) {
-      cache.set(`${i}:${j}`, dfs(i + 1, j + 1))
-      return cache.get(`${i}:${j}`)
+      cache.set(`${i}:${j}`, dfs(i + 1, j + 1));
+      return cache.get(`${i}:${j}`);
     }
 
-    cache.set(`${i}:${j}`, false)
-    return false
+    cache.set(`${i}:${j}`, false);
+    return false;
   }
-
 
   return dfs(0, 0);
 };
@@ -150,94 +153,106 @@ var maxArea = function (height) {
 
   // return res
 
-  let res = 0
-  let l = 0
-  let r = height.length - 1
+  let res = 0;
+  let l = 0;
+  let r = height.length - 1;
 
   while (l < r) {
-    let area = (r - l) * Math.min(height[l], height[r])
-    res = Math.max(res, area)
+    let area = (r - l) * Math.min(height[l], height[r]);
+    res = Math.max(res, area);
 
-    if (height[l] < height[r]) { l += 1 }
-    else { r -= 1 }
+    if (height[l] < height[r]) {
+      l += 1;
+    } else {
+      r -= 1;
+    }
   }
 
-  return res
+  return res;
 };
 
 var intToRoman = function (num) {
   let symList = [
-    [1, 'I'],
-    [4, 'IV'],
-    [5, 'V'],
-    [9, 'IX'],
-    [10, 'X'],
-    [40, 'XL'],
-    [50, 'L'],
-    [90, 'XC'],
-    [100, 'C'],
-    [400, 'CD'],
-    [500, 'D'],
-    [900, 'CM'],
-    [1000, 'M']
-  ]
+    [1, "I"],
+    [4, "IV"],
+    [5, "V"],
+    [9, "IX"],
+    [10, "X"],
+    [40, "XL"],
+    [50, "L"],
+    [90, "XC"],
+    [100, "C"],
+    [400, "CD"],
+    [500, "D"],
+    [900, "CM"],
+    [1000, "M"],
+  ];
 
-  let res = ''
+  let res = "";
   for (let [val, sym] of symList.reverse()) {
-    let count = Math.floor(num / val)
+    let count = Math.floor(num / val);
     if (count) {
-      res += sym.repeat(count)
-      num = num % val
+      res += sym.repeat(count);
+      num = num % val;
     }
   }
 
-  return res
+  return res;
 };
 
-
 var romanToInt = function (s) {
-  let symList = new Map()
-  symList.set('I', 1)
-  symList.set('IV', 4)
-  symList.set('V', 5)
-  symList.set('IX', 9)
-  symList.set('X', 10)
-  symList.set('XL', 40)
-  symList.set('L', 50)
-  symList.set('XC', 90)
-  symList.set('C', 100)
-  symList.set('CD', 400)
-  symList.set('D', 500)
-  symList.set('CM', 900)
-  symList.set('M', 1000)
+  let symList = new Map();
+  symList.set("I", 1);
+  symList.set("IV", 4);
+  symList.set("V", 5);
+  symList.set("IX", 9);
+  symList.set("X", 10);
+  symList.set("XL", 40);
+  symList.set("L", 50);
+  symList.set("XC", 90);
+  symList.set("C", 100);
+  symList.set("CD", 400);
+  symList.set("D", 500);
+  symList.set("CM", 900);
+  symList.set("M", 1000);
 
   let res = 0;
   for (let i = 0; i < s.length; i++) {
     if (symList.has(s.substring(i, i + 2))) {
-      res += symList.get(s.substring(i, i + 2))
-      i++
+      res += symList.get(s.substring(i, i + 2));
+      i++;
     } else if (symList.has(s[i])) {
-      res += symList.get(s[i])
+      res += symList.get(s[i]);
     }
   }
 
-  return res
+  return res;
 };
 
 var longestCommonPrefix = function (strs) {
-  let res = ''
+  let res = "";
 
   for (let i = 0; i < strs[0].length; i++) {
     for (let s of strs) {
       if (i == s.length || s[i] !== strs[0][i]) {
-        return res
+        return res;
       }
     }
 
-    res += strs[0][i]
+    res += strs[0][i];
   }
 
-  return res
+  return res;
 };
 
-export { convert, reverse, myAtoi, isPalindrome, isMatch, maxArea, intToRoman, romanToInt, longestCommonPrefix }
+export {
+  convert,
+  reverse,
+  myAtoi,
+  isPalindrome,
+  isMatch,
+  maxArea,
+  intToRoman,
+  romanToInt,
+  longestCommonPrefix,
+};
--- top100/medium/index.test.js
+++ top100/medium/index.test.js
@@ -1,75 +1,82 @@
-import { convert, intToRoman, isMatch, isPalindrome, longestCommonPrefix, maxArea, myAtoi, reverse, romanToInt } from './';
+import {
+  convert,
+  intToRoman,
+  isMatch,
+  isPalindrome,
+  longestCommonPrefix,
+  maxArea,
+  myAtoi,
+  reverse,
+  romanToInt,
+  threeSum,
+} from "./";
 
-describe('6. Zigzag Conversion', () => {
-    test('should return conversion', () => {
-        expect(convert("PAYPALISHIRING", 3)).toBe("PAHNAPLSIIGYIR")
-    });
+describe("6. Zigzag Conversion", () => {
+  test("should return conversion", () => {
+    expect(convert("PAYPALISHIRING", 3)).toBe("PAHNAPLSIIGYIR");
+  });
 });
 
-
-describe('7. Reverse Integer', () => {
-    test('should return reversed integer', () => {
-        expect(reverse(123)).toBe(321);
-        expect(reverse(-123)).toBe(-321);
-        expect(reverse(120)).toBe(21);
-    });
+describe("7. Reverse Integer", () => {
+  test("should return reversed integer", () => {
+    expect(reverse(123)).toBe(321);
+    expect(reverse(-123)).toBe(-321);
+    expect(reverse(120)).toBe(21);
+  });
 });
 
-
-describe('8. String to Integer (atoi)', () => {
-    test('should return converted number', () => {
-        expect(myAtoi("42")).toBe(42);
-        expect(myAtoi("   -42")).toBe(-42);
-        expect(myAtoi("4193 with words")).toBe(4193);
-    });
+describe("8. String to Integer (atoi)", () => {
+  test("should return converted number", () => {
+    expect(myAtoi("42")).toBe(42);
+    expect(myAtoi("   -42")).toBe(-42);
+    expect(myAtoi("4193 with words")).toBe(4193);
+  });
 });
 
-describe('9. Palindrome Number', () => {
-    test('should check given number is palindrome or not', () => {
-        expect(isPalindrome(121)).toBe(true);
-        expect(isPalindrome(-121)).toBe(false);
-        expect(isPalindrome(10)).toBe(false);
-        expect(isPalindrome(1221)).toBe(true);
-    });
+describe("9. Palindrome Number", () => {
+  test("should check given number is palindrome or not", () => {
+    expect(isPalindrome(121)).toBe(true);
+    expect(isPalindrome(-121)).toBe(false);
+    expect(isPalindrome(10)).toBe(false);
+    expect(isPalindrome(1221)).toBe(true);
+  });
 });
 
-
-describe('10. Regular Expression Matching', () => {
-    test('should check given string and path are matching or not', () => {
-        expect(isMatch("aa", "a")).toBe(false);
-        expect(isMatch("aa", "a*")).toBe(true);
-    });
+describe("10. Regular Expression Matching", () => {
+  test("should check given string and path are matching or not", () => {
+    expect(isMatch("aa", "a")).toBe(false);
+    expect(isMatch("aa", "a*")).toBe(true);
+  });
 });
 
-describe('11. Container With Most Water', () => {
-    test('should calculate max area', () => {
-        expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49)
-        expect(maxArea([1, 1])).toBe(1)
-    });
+describe("11. Container With Most Water", () => {
+  test("should calculate max area", () => {
+    expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
+    expect(maxArea([1, 1])).toBe(1);
+  });
 });
 
-
-describe('12. Integer to Roman', () => {
-    test('should return converted roman number', () => {
-        expect(intToRoman(3)).toBe("III");
-        expect(intToRoman(58)).toBe("LVIII");
-        expect(intToRoman(1994)).toBe("MCMXCIV");
-        expect(intToRoman(6)).toBe("VI");
-    });
+describe("12. Integer to Roman", () => {
+  test("should return converted roman number", () => {
+    expect(intToRoman(3)).toBe("III");
+    expect(intToRoman(58)).toBe("LVIII");
+    expect(intToRoman(1994)).toBe("MCMXCIV");
+    expect(intToRoman(6)).toBe("VI");
+  });
 });
 
-describe('13. Roman to Integer', () => {
-    test('should return converted integer number', () => {
-        expect(romanToInt("III")).toBe(3)
-        expect(romanToInt("LVIII")).toBe(58)
-        expect(romanToInt("MCMXCIV")).toBe(1994)
-    });
+describe("13. Roman to Integer", () => {
+  test("should return converted integer number", () => {
+    expect(romanToInt("III")).toBe(3);
+    expect(romanToInt("LVIII")).toBe(58);
+    expect(romanToInt("MCMXCIV")).toBe(1994);
+  });
 });
 
-
-describe('14. Longest Common Prefix', () => {
-    test('should return longest prefix', () => {
-        expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe('fl')
-        expect(longestCommonPrefix(["dog","racecar","car"])).toBe('')
-    });
+describe("14. Longest Common Prefix", () => {
+  test("should return longest prefix", () => {
+    expect(longestCommonPrefix(["flower", "flow", "flight"])).toBe("fl");
+    expect(longestCommonPrefix(["dog", "racecar", "car"])).toBe("");
+  });
 });
+
