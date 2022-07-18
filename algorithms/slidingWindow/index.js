ed6e51e57dee52ac07b72b962fa0107265175139
Guven Karanfil <guven.karanfil@smartface.io>
2022-07-18 18:57:06 +0300

    sliding window technique

---
+++ algorithms/slidingWindow/index.js
@@ -0,0 +1,56 @@
+var findMaxAverage = function (nums, k) {
+  let total = Number.MIN_SAFE_INTEGER;
+  let runningSum = 0;
+
+  for (let index = 0; index < k; index++) {
+    runningSum += nums[index];
+  }
+
+  total = Math.max(runningSum, total);
+
+  let slow = 0;
+  for (let fast = k; fast < nums.length; fast++) {
+    runningSum += nums[fast];
+    runningSum -= nums[slow];
+
+    total = Math.max(runningSum, total);
+
+    slow++;
+  }
+
+  return total / k;
+};
+
+var longestSubstring = function (s, k) {
+  if (k === 1) return s.length;
+
+  let maxUnique = new Set(s);
+  let maxLen = 0;
+
+  for (let i = 1; i <= maxUnique.size; i++) {
+    const freq = new Map();
+    let start = 0,
+      currUnique = 0,
+      countAtLeastK = 0;
+
+    for (let end = 0; end < s.length; end++) {
+      freq.set(s[end], (freq.get(s[end]) || 0) + 1);
+
+      if (freq.get(s[end]) === k) countAtLeastK++;
+      if (freq.get(s[end]) === 1) currUnique++;
+
+      while (currUnique > i) {
+        if (freq.get(s[start]) === k) countAtLeastK--;
+        freq.set(s[start], freq.get(s[start]) - 1);
+        if (!freq.get(s[start])) currUnique--;
+        start++;
+      }
+      if (currUnique === countAtLeastK) {
+        maxLen = Math.max(maxLen, end - start + 1);
+      }
+    }
+  }
+  return maxLen;
+};
+
+export { findMaxAverage, longestSubstring };
---
+++ algorithms/slidingWindow/index.test.js
@@ -0,0 +1,15 @@
+import { findMaxAverage, longestSubstring } from "./";
+
+describe("Sliding Window", () => {
+  test("findMaxAverage", () => {
+    expect(findMaxAverage([1, 12, -5, -6, 50, 3], 4)).toBe(12.75);
+    expect(findMaxAverage([0, 4, 0, 3, 2], 1)).toBe(4);
+    expect(findMaxAverage([5], 1)).toBe(5);
+    expect(findMaxAverage([-1], 1)).toBe(-1);
+  });
+
+  test("longestSubstring", () => {
+    expect(longestSubstring("aaabb", 3)).toBe(3);
+    expect(longestSubstring("pababbc", 2)).toBe(5);
+  });
+});
