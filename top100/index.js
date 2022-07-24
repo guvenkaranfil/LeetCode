9e0406b7ffe3c3ae2833fb3647d2c243b4bd1ea8
Güven Karanfil <guvenkaranfil235@gmail.com>
2022-07-24 17:30:46 +0300

    merge easy&medium problems into one file for top100 leetcode problems

--- top100/index.js
+++ top100/index.js
@@ -1,4 +1,64 @@
-import { LinkedList } from "../../dataStructures/linkedList";
+var twoSum = function (nums, target) {
+  let prevMap = {}; // val : index
+
+  for (let index = 0; index < nums.length; index++) {
+    const number = nums[index];
+    const diff = target - number;
+
+    if (diff in prevMap) {
+      return [prevMap[diff], index];
+    } else {
+      prevMap[number] = index;
+    }
+  }
+};
+
+import { LinkedListNode } from "../dataStructures/linkedList";
+const addTwoNumbers = function (l1, l2) {
+  let head = new LinkedListNode();
+  let curr = head;
+
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
+
+    carry = parseInt(sum / 10);
+    sum = sum % 10;
+    curr.next = new LinkedListNode(sum);
+
+    curr = curr.next;
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
+  }
+
+  return head.next;
+};
+
+var removeElement = function (nums, val) {
+  let k = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+};
+
+var removeDuplicates = function (nums) {
+  let k = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[k] !== nums[i]) {
+      k++;
+      nums[k] = nums[i];
+    }
+  }
+
+  return k + 1;
+};
 
 const lengthOfLongestSubstring = function (s) {
   let aPointer = 0;
@@ -532,6 +592,10 @@ var fourSum = function (nums, target) {
 };
 
 export {
+  twoSum,
+  addTwoNumbers,
+  removeElement,
+  removeDuplicates,
   lengthOfLongestSubstring,
   findMedianSortedArrays,
   convert,
--- top100/index.test.js
+++ top100/index.test.js
@@ -1,5 +1,9 @@
-import { LinkedList } from "../../dataStructures/linkedList";
+import { LinkedList } from "../dataStructures/linkedList";
 import {
+  twoSum,
+  addTwoNumbers,
+  removeElement,
+  removeDuplicates,
   convert,
   findMedianSortedArrays,
   fourSum,
@@ -19,6 +23,59 @@ import {
   threeSum,
   threeSumClosest,
 } from "./";
+
+describe("1. Two Sum", () => {
+  test("should get the target indices of [1,2] ", () => {
+    const indices = twoSum([2, 1, 5, 3], 4);
+
+    expect(indices).toStrictEqual([1, 3]);
+  });
+  test("should get the target indices of [2,7,11,15] [0,1] ", () => {
+    const indices = twoSum([2, 7, 11, 15], 9);
+
+    expect(indices).toStrictEqual([0, 1]);
+  });
+});
+
+describe("2. Add Two Numbers", () => {
+  test("should return sum of two linked list sample1", () => {
+    const linkedList = new LinkedList();
+    const list1 = linkedList.fromArray([2, 4, 3]);
+    const list2 = linkedList.fromArray([5, 4, 4]);
+    const resultLinkedList = linkedList.fromArray([7, 8, 7]);
+
+    const result = addTwoNumbers(list1, list2);
+    expect(result).toEqual(resultLinkedList);
+  });
+
+  test("should return sum of two linked list sample2", () => {
+    const linkedList = new LinkedList();
+    const list1 = linkedList.fromArray([2, 4, 3]);
+    const list2 = linkedList.fromArray([5, 6, 4]);
+    const resultLinkedList = linkedList.fromArray([7, 0, 8]);
+
+    const result = addTwoNumbers(list1, list2);
+    expect(result).toEqual(resultLinkedList);
+  });
+
+  test("should return sum of two linked list sample3", () => {
+    const linkedList = new LinkedList();
+    const list1 = linkedList.fromArray([7]);
+    const list2 = linkedList.fromArray([8]);
+    const resultLinkedList = linkedList.fromArray([5, 1]);
+
+    const result = addTwoNumbers(list1, list2);
+    expect(result).toEqual(resultLinkedList);
+  });
+
+  test("27. removeElement", () => {
+    expect(removeElement([3, 2, 2, 3], 3)).toBe(2);
+  });
+});
+
+test("26. Remove Duplicates from Sorted Array", () => {
+  expect(removeDuplicates([1, 1, 2])).toBe(2);
+});
 
 describe("3. Longest Substring Without Repeating Characters", () => {
   test("should return longest non repeating character", () => {
